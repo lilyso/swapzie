3a8b8617a35cf5b4fbc911df3f8f8afb2cd4f2c2
lilys0 <lsouryaphanh@gmail.com>
2022-01-31 11:49:57 +1100

    modal set up

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -4,39 +4,71 @@ import { Link as RouterLink } from "react-router-dom";
 import { Tabs, TabList, Tab, Image, Flex, Spacer, Box } from "@chakra-ui/react";
 
 function Nav() {
-  return (
-    <header>
-      <Box p={4} bgColor="#f7fff7">
-        <Flex>
-          <Image
-            src="./images/swapzie-logo-200.png"
-            alt="swapzie logo"
-            h="100"
-          ></Image>
-          <Spacer />
-          <Tabs color="#1a535c" variant="unstyled" pt={8}>
-            <TabList>
-              <Tab>
-                <RouterLink to="/about">About</RouterLink>
-              </Tab>
-              <Tab>
-                <RouterLink to="/swap">Swap</RouterLink>
-              </Tab>
-              <Tab>
-                <RouterLink to="/dashboard">Dashboard</RouterLink>
-              </Tab>
-              <Tab>
-                <RouterLink to="/login">Login/Sign Up</RouterLink>
-              </Tab>
-              <Tab>
-                <RouterLink to="/logout">Logout</RouterLink>
-              </Tab>
-            </TabList>
-          </Tabs>
-        </Flex>
-      </Box>
-    </header>
-  );
+  if (Auth.loggedIn()) {
+    return (
+      <header>
+        <Box p={4} bgColor="#f7fff7">
+          <Flex>
+            <Image
+              src="./images/swapzie-logo-200.png"
+              alt="swapzie logo"
+              h="100"
+            ></Image>
+            <Spacer />
+            <Tabs color="#1a535c" variant="unstyled" pt={8}>
+              <TabList>
+                <Tab>
+                  <RouterLink to="/about">About</RouterLink>
+                </Tab>
+                <Tab>
+                  <RouterLink to="/swap">Swap</RouterLink>
+                </Tab>
+                <Tab>
+                  <RouterLink to="/dashboard">Dashboard</RouterLink>
+                </Tab>
+                <Tab>
+                  <RouterLink to="/login">Login/Sign Up</RouterLink>
+                </Tab>
+                <Tab>
+                  <a href="/" onClick={() => Auth.logout()}>
+                    Logout
+                  </a>
+                </Tab>
+              </TabList>
+            </Tabs>
+          </Flex>
+        </Box>
+      </header>
+    );
+  } else {
+    return (
+      <header>
+        <Box p={4} bgColor="#f7fff7">
+          <Flex>
+            <Image
+              src="./images/swapzie-logo-200.png"
+              alt="swapzie logo"
+              h="100"
+            ></Image>
+            <Spacer />
+            <Tabs color="#1a535c" variant="unstyled" pt={8}>
+              <TabList>
+                <Tab>
+                  <RouterLink to="/about">About</RouterLink>
+                </Tab>
+                <Tab>
+                  <RouterLink to="/swap">Swap</RouterLink>
+                </Tab>
+                <Tab>
+                  <RouterLink to="/login">Login/Sign Up</RouterLink>
+                </Tab>
+              </TabList>
+            </Tabs>
+          </Flex>
+        </Box>
+      </header>
+    );
+  }
 }
 
 export default Nav;
