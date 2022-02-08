34d8f7ed2276e90559725f8b3c6ed03a412a5368
lilys0 <lsouryaphanh@gmail.com>
2022-02-08 12:57:26 +1100

    seed fix

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -17,6 +17,8 @@ db.once("open", async () => {
       { name: "Dolls" },
       { name: "Action Figures" },
       { name: "Pretend Play" },
+      { name: "Cars & Vehicles" },
+      { name: "Miscellaneous" },
     ]);
 
     console.log("categories seeded");
@@ -27,84 +29,73 @@ db.once("open", async () => {
         description:
           "Well-loved toy kitchen. Comes with toy kitchen accessories and food.",
         image: "",
-        category: "Pretend Play",
+        age: "4-5yo",
+        category: { _id: "", name: "Pretend Play" },
+        location: "Sydney",
         comments: [],
+        user: "",
+        claimedBy: "",
       },
       {
         title: "Marvel action heroes",
         description:
           "Avengers actions heroes. Great condition. Small scratch on Spiderman.",
         image: "",
-        category: "Action Figures",
+        age: "4-5yo",
+        category: { _id: "", name: "Action Figures" },
+        location: "Sydney",
         comments: [],
+        user: "",
+        claimedBy: "",
       },
       {
-        title: "Various baby books",
+        title: "Various toddler books",
         description: "All in great condition and barely used",
         image: "",
-        category: "Books",
+        age: "1-2yo",
+        category: { _id: "", name: "Books" },
+        location: "Sydney",
         comments: [],
+        user: "",
+        claimedBy: "",
       },
       {
         title: "Wooden puzzles for toddlers",
         description:
           "My 2yo loved these but she's grown out of them now. Good used condition and many more years to go.",
-        image: "",
-        category: "Puzzles",
+        image: "1-2yo",
+        category: { _id: "", name: "Puzzles" },
+        location: "Sydney",
         comments: [],
+        user: "",
+        claimedBy: "",
       },
       {
         title: "100+ set of magnetic tiles",
         description:
           "Keep the kids busy with these magnetic tiles. A few scuffs but in good working condition",
         image: "",
-        category: "Construction",
+        category: { _id: "", name: "Construction" },
+        location: "Sydney",
         comments: [],
+        user: "",
+        claimedBy: "",
       },
       {
         title: "Set of Frozen dolls",
         description:
           "She got two sets for christmas so these have barely been played with.",
         image: "",
-        category: "Dolls",
+        age: "4-5yo",
+        category: { _id: "", name: "Dolls" },
+        location: "Sydney",
         comments: [],
+        user: "",
+        claimedBy: "",
       },
     ]);
 
     console.log("posts seeded");
-
-    const users = await User.insertMany([
-      {
-        firstName: "Lily",
-        lastName: "So",
-        email: "lily@testmail.com",
-        password: "password12345",
-        posts: [],
-      },
-      {
-        firstName: "Sarah",
-        lastName: "Head",
-        email: "sarah@testmail.com",
-        password: "password12345",
-        posts: [],
-      },
-      {
-        firstName: "Breana",
-        lastName: "Bee",
-        email: "bre@testmail.com",
-        password: "password12345",
-        posts: [],
-      },
-      {
-        firstName: "Nick",
-        lastName: "La",
-        email: "nick@testmail.com",
-        password: "password12345",
-        posts: [],
-      },
-    ]);
-
-    console.log("users seeded");
 
     process.exit();
   } catch (err) {
